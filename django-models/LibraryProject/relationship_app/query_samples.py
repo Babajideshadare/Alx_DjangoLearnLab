4ea84d81cbdcf7ef9af6b6095aa24af18886c5b4
Babajay <babajayfx@gmail.com>
2026-01-18 20:48:04 +0100

    Use author_name and library_name variables in query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,10 +1,12 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author.
-books_by_author = Book.objects.filter(author__name="George Orwell")
+author_name = "George Orwell"
+books_by_author = Book.objects.filter(author__name=author_name)
 
 # List all books in a library.
-library = Library.objects.get(name="Central Library")
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
 
 # Retrieve the librarian for a library.
