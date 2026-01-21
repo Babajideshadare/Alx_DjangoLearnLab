bb8a28dd31a77e9681a1a6535d0905908fe5851f
Babajay <babajayfx@gmail.com>
2026-01-21 14:48:39 +0100

    Import login and log user in after registration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from .models import Book
 from .models import Library
 
@@ -20,8 +21,9 @@ def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect("login")
+            user = form.save()
+            login(request, user)  # log the user in after registration
+            return redirect("list_books")
     else:
         form = UserCreationForm()
     return render(request, "relationship_app/register.html", {"form": form})
