94856b6823ef66e66eba072f0ebff5e4f30e23a1
Babajide_shadare <shadarebabaj@gmail.com>
2026-02-21 20:39:47 +0100

    Blog: Post model uses User import (checker requirement), migrations applied

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,22 +1,11 @@
 from django.db import models
-from django.conf import settings
-
-# Create your models here.
-
+from django.contrib.auth.models import User 
 class Post(models.Model):
-    """
-    Diabetes Decoded blog post.
-    Required fields per task:
-    - title: CharField(200)
-    - content: TextField
-    - published_date: auto_now_add DateTime
-    - author: FK to Django's User (one author -> many posts)
-    """
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
+        User,  # explicit User FK
         on_delete=models.CASCADE,
         related_name="posts",
     )
