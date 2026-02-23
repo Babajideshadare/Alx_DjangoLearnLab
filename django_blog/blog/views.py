70757a249642544d43466f770e33a9c40101108c
Babajide_shadare <shadarebabaj@gmail.com>
2026-02-23 15:26:07 +0100

    Implement profile view to edit user email via POST

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .forms import ProfileForm
 
-# Create your views here.
+
+def register(request):
+    return render(request, 'blog/register.html')
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+    else:
+        form = ProfileForm(instance=request.user)
+    return render(request, 'blog/profile.html', {'form': form})
